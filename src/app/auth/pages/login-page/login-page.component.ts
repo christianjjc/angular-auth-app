1bb6aa9c7bd9e64a2e40a44d55fb80937d04d2e8
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-30 09:54:27 -0500

    seccion-26-ep-11-2-sweet-alert

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import Swal from 'sweetalert2';
+
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -21,7 +24,9 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
     this.authService.login(email, password).subscribe({
       next: () => console.log('todo bien'),
-      error: (error) => console.log({ loginError: error }),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      },
     });
   }
 }
